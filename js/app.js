8537ca92226fb37293bc6e6072fa4608029048f5
Matt <mattbledsoe2@gmail.com>
2017-06-13 11:32:01 -0400

    add speed
    added random speed and new player position
    also added changes from meeting with Karol

--- js/app.js
+++ js/app.js
@@ -10,11 +10,14 @@ var playerH = 83;
 var playerW = 68;
 var enemyH = 64;
 var enemyW = 96;
-
+var initialPositions = [60, 145, 225];
+var eSpeed = [2, 4, 6, 8];
+var randomSpeed = eSpeed[Math.floor(Math.random() * eSpeed.length)];
+// var
 var chars = [
   'images/char-boy.png',
   'images/char-pink-girl.png',
-  'images/char-princess-girl.png',  //check this
+  'images/char-princess-girl.png',
   'images/char-cat-girl.png',
   'images/char-horn-girl.png'
 ];
@@ -26,7 +29,7 @@ var randChar = chars[Math.floor(Math.random() * chars.length)];
 var Enemy = function(x, y, speed) {
   this.x = x; //horizontal position
   this.y = y; //verticle position
-  this.speed = speed;
+  this.speed = randomSpeed;
   this.sprite = 'images/enemy-bug.png';
 };
 
@@ -37,15 +40,25 @@ var Enemy = function(x, y, speed) {
 // all computers.
 
 Enemy.prototype.update = function(dt) {
-  this.x = this.x + this.speed * dt;
-  if (this.x <= xLimit) {                //check this
-    this.x = enemyStart;
-  }
+
+  this.x += this.speed;
+ // this.x = this.x + this.speed * dt;
+ // if (this.x <= xLimit) {                //check this
+ //   this.x = enemyStart;
+ // }
+
+  this.checkCollision();
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+  //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  // https://developer.mozilla.org/en/docs/Web/API/CanvasRenderingContext2D/drawImage
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.checkCollision = function() {
+  //console.log("checkCollision");
 };
 
 var collision = function() {
@@ -54,13 +67,11 @@ var collision = function() {
   }                                   // JS Hint- 53	Expected an assignment or function
 };                                       // call and instead saw an expression.
 
-var Player = function(x, y, speed) {
+var Player = function(x, y) {
   this.x = x;
   this.y = y;
-  this.speed = speed;
   // this.sprite = 'images/char-boy.png';
   this.sprite = randChar; //this may be a mistake and need to be fixed with Karol
-
 };
 
 // Parameter: dt, a time delta between ticks
@@ -68,23 +79,24 @@ Player.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
-  this.x = this.x * dt;
-  this.y = this.y * dt;
-  if (this.y <= yLimit) { //reaches verticle limit
-    this.restart();
-  }
+  // this.x = this.x * dt;
+  // this.y = this.y * dt;
+  // if (this.y <= yLimit) { //reaches verticle limit
+  //   this.restart();
+  // }
 };
 
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.restart = function() {
-  this.x = playerX;
-  this.y = playerY;
-};
+// Player.prototype.restart = function() {
+//   this.x = playerX;
+//   this.y = playerY;
+// };
 
 Player.prototype.handleInput = function(input) {
+  //console.log(input);
   if (this.x > 0 && input === "left") {
     this.x -= blockWidth;
   }
@@ -99,15 +111,19 @@ Player.prototype.handleInput = function(input) {
   }
 };
 
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
-for (var i = 0; i < 3; i++) {                       //check that everything is
-  allEnemies[allEnemies.length] = new Enemy();      //  being called
+for (var i = 0; i < 3; i++) {
+  // Next step: different x and y coordinates, use, for example, randomness
+  //allEnemies[i] = new Enemy(10,10,10);      //  being called
+  allEnemies[i] = new Enemy(-100, initialPositions[i], 50);      //  being called
 }
 allEnemies.push();
 // Place the player object in a variable called player
-var player = new Player();
+var player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
